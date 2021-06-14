01a7d0a4c0c2bdcb6fe89f6b5208fbb1300cdc0e
wesleyluam <84480216+wesleyluam@users.noreply.github.com>
2021-06-14 12:59:45 -0300

    Update Aula02InicializandoArraysMultidimensionais.java

--- Aula02InicializandoArraysMultidimensionais.java
+++ Aula02InicializandoArraysMultidimensionais.java
@@ -5,12 +5,6 @@ import java.util.Scanner;
 public class Aula02InicializandoArraysMultidimensionais {
     public static void main(String[] args) {
         int [][] arrayMulti1 = new int[2][3];
-//        arrayMulti1[0][0] = 22;
-//        arrayMulti1[0][1] = 11;
-//        arrayMulti1[0][2] = 54;
-//        arrayMulti1[1][0] = 44;
-//        arrayMulti1[1][1] = 55;
-//        arrayMulti1[1][2] = 76;
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < arrayMulti1.length; i++) {
             for (int j = 0; j < arrayMulti1.length; j++) {
